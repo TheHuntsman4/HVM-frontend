4eaf49213e51a89c01b41e20feaebc2bd07df194
TheHuntsman4 <anikethvij464@gmail.com>
2023-10-21 13:32:30 +0530

    <feat>:[UI overhaul and makes the app somewhat responisve]

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -5,6 +5,7 @@ import { useForm } from "react-hook-form";
 import { FormComponent } from "../components";
 import addSVG from "../assets/add.svg";
 import cameraSVG from '../assets/camera.svg';
+import placeHolder from '../assets/placeholder.jpeg'
 export const Mainform = () => {
 
   // form hook definitions
@@ -47,71 +48,80 @@ export const Mainform = () => {
     <div className="h-full w-full">
       <form onSubmit={handleSubmit(onSubmit)} className="h-full w-full flex flex-col items-center mt-24">
         <div className="p-6 w-2/3 bg-[#E9EDFF]">
-          <div className="h-1/2 p-12 grid grid-cols-1 w-1/2">
-            <label className="font-semibold text-md">Full Name</label>
-            <input type="text" id="leadFullName"  {...register('leadFullName')} className="h-10 px-2" />
-            <label className="font-semibold text-md pt-6 pb-2">Company Name</label>
-            <input type="text" id='companyName' {...register('companyName')} className=" h-10 px-2" />
-            <label className="font-semibold text-md pt-6 pb-2">Phone Number</label>
-            <input type="number" id='leadPhoneNumber' {...register('leadPhoneNumber')} className=" h-10 px-2" />
-            <label className="font-semibold text-md pt-6 pb-2">Email</label>
-            <input type="email" id='leadEmail' {...register('leadEmail')} className=" h-10 px-2" />
-            <input type="hidden" {...register('leadImage')} />
+          <div className="h-1/2 p-12 w-full flex">
+            <div className="flex flex-col w-1/2">
+              <label className="font-semibold text-md">Full Name</label>
+              <input type="text" id="leadFullName"  {...register('leadFullName')} className="h-10 px-2" />
+              <label className="font-semibold text-md pt-6 pb-2">Company Name</label>
+              <input type="text" id='companyName' {...register('companyName')} className=" h-10 px-2" />
+              <label className="font-semibold text-md pt-6 pb-2">Phone Number</label>
+              <input type="number" id='leadPhoneNumber' {...register('leadPhoneNumber')} className=" h-10 px-2" />
+              <label className="font-semibold text-md pt-6 pb-2">Email</label>
+              <input type="email" id='leadEmail' {...register('leadEmail')} className=" h-10 px-2" />
+            </div>
+            <div className="w-1/2">
+              <input type="hidden" {...register('leadImage')} />
+              {/* WEBCAM COMPONENT  */}
+              {showModal && (
+                <div
+                  className="absolute top-0 left-0 h-screen w-full flex flex-col justify-center items-center bg-opacity-75 bg-black"
+                  onClick={toggleModal}
+                >
+                  <div className="h-auto flex flex-col justify-center items-center w-1/3 bg-slate-600 rounded-md p-4">
+                    <Webcam
+                      ref={webCamRef}
+                      audio={false}
+                      className="w-full h-full"
+                      screenshotFormat="image/jpeg"
+                    ></Webcam>
+                    <button
+                      className=" w-1/3  flex justify-evenly items-center py-4 px-2 mt-4 rounded-lg bg-amber-600"
+                      onClick={captureWebcam}
+                    >
+                      <img src={cameraSVG} width={40} className=""></img>
+                      Capture image
+                    </button>
+                  </div>
+                </div>
+              )}
+              <div className="flex flex-col   justify-center items-center w-full">
+                {imageSrc != 0 ?
+                  <div className="flex flex-col justify-center items-center">
+                    <img src={imageSrc} width={360} alt="Profile Picture" />
+                    <button
+                      className="h-auto w-1/2 mt-4 p-2 rounded-lg bg-amber-600 "
+                      onClick={toggleModal}
+                    >
+                      Take Picture Again
+                    </button>
+                  </div> :
+                  <div className="flex flex-col justify-center items-center">
+                    <img src={placeHolder} width={240} alt="Profile Picture" />
+                    <button
+                      className="h-auto w-1/2 mt-4 p-2 rounded-lg bg-amber-600 "
+                      onClick={toggleModal}
+                    >
+                      Take Picture
+                    </button>
+                  </div>}
+              </div>
+            </div>
+          </div>
+          <div className="w-full flex pl-12">
+            <div className="w-1/2 flex flex-col">
+              <label className="font-semibold text-md">Address Line 1</label>
+              <input type="text" {...register('leadAddress1')} className="w-3/4 h-10 px-2" />
+            </div>
+            <div className="w-1/2 flex flex-col">
+              <label className="font-semibold text-md">Address Line 2</label>
+              <input type="text" {...register('leadAddress2')} className="w-3/4 h-10 px-2"/>
+            </div>
           </div>
         </div>
         <span>File Upload</span>
         <div className="p-6 w-2/3 bg-[#E9EDFF]">
-          <input type="hidden" {...register('leadImage')} />
-          {/* WEBCAM COMPONENT  */}
-          {showModal && (
-            <div
-              className="absolute top-0 left-0 h-screen w-full flex flex-col justify-center items-center bg-opacity-75 bg-black"
-              onClick={toggleModal}
-            >
-              <div className="h-auto flex flex-col justify-center items-center w-1/3 bg-slate-600 rounded-md p-4">
-                <Webcam
-                  ref={webCamRef}
-                  audio={false}
-                  className="w-full h-full"
-                  screenshotFormat="image/jpeg"
-                ></Webcam>
-                <button
-                  className=" w-1/3  flex justify-evenly items-center py-4 px-2 mt-4 rounded-lg bg-amber-600"
-                  onClick={captureWebcam}
-                >
-                  <img src={cameraSVG} width={40} className=""></img>
-                  Capture image
-                </button>
-              </div>
-            </div>
-          )}
-          <img src={imageSrc} width={360} alt="Profile Picture" />
-          {imageSrc != 0 ? <button
-            className="h-auto w-1/3 p-2 rounded-lg bg-amber-600 "
-            onClick={toggleModal}
-          >
-            Take Picture Again 
-          </button> :
-            <button
-              className="h-auto w-1/3 p-2 rounded-lg bg-amber-600 "
-              onClick={toggleModal}
-            >
-              Take Picture
-            </button>}
 
         </div>
-
-
-
-
-        {/* Addresss stuff   */}
-        {/* <span className="">
-    <label className="font-semibold text-md pt-6 pb-2">Address Line 1</label>
-    <input type="leadAddress1" id='leadAddress1' {...register('leadAddress1')} className=" h-10 px-2" />
-    <label className="font-semibold text-md pt-6 pb-2">Address Line 2</label>
-    <input type="leadAddress2" id='leadAddress2' {...register('leadAddress2')} className=" h-10 px-2" />
-  </span> */}
-
 
         <button
           className="bg-amber-600 font-semibold rounded-lg px-12 py-4"
