6e0f218e173fed0163986a9c3db1b34a27b532e6
TheHuntsman4 <anikethvij464@gmail.com>
2023-10-26 16:19:47 +0530

    <feat>:[adds the printing function to the home page]

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router";
+import printerSVG from "../assets/printer.svg";
 import { Navbar } from "../components";
+import { click } from "@testing-library/user-event/dist/click";
 
 const Home = () => {
   const [data, setData] = useState([]);
   const currentUser = localStorage.getItem("current_user");
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,6 +32,21 @@ const Home = () => {
 
     fetchData();
   }, []);
+  function formatDateTime(dateStr) {
+    const dateObj = new Date(dateStr);
+
+    const day = String(dateObj.getDate()).padStart(2, "0");
+    const month = String(dateObj.getMonth() + 1).padStart(2, "0"); // January is 0!
+    const year = dateObj.getFullYear();
+
+    const hours = String(dateObj.getHours()).padStart(2, "0");
+    const minutes = String(dateObj.getMinutes()).padStart(2, "0");
+
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+  }
+  const handlePrint = (uuid) =>{
+navigate("/print",{ state: { uuid: uuid } })
+  }
   return (
     <div className="h-full w-full">
       <div className="mx-64 mt-12">
@@ -58,7 +77,15 @@ const Home = () => {
               <div>
                 {item.visiting_date} {item.visiting_time}
               </div>
-              <div>{item.valid_till}</div>
+              <div className="grid grid-cols-2 gap-2">
+                <div>{formatDateTime(item.valid_till)}</div>
+                <img
+                  src={printerSVG}
+                  width={20}
+                  onClick={() => handlePrint(item.unique_id)
+                  }
+                />
+              </div>
             </React.Fragment>
           ))}
         </div>
